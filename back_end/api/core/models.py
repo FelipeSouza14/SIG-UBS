5603c4c931d2506fa124410395046b913c99c80a
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-07-09 11:28:29 -0300

    Criando a primeira parte dos models

--- back_end/api/core/models.py
+++ back_end/api/core/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+# Modelos do Banco de Dados
+class Administrador(models.Model):
+    cnpj = models.CharField(max_length=14, unique=True)
+    senha = models.CharField(max_length=50, validators=[MinLengthValidator(8)])
+
+    def __str__(self):
+        return self.cnpj
+
+
+class Paciente(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
+    numTelefone = models.CharField(max_length=11, unique=True)
+    dataNascimento = models.DateField(max_length=15)
+    senha = models.CharField(max_length=50, validators=[MinLengthValidator(8)])
+
+    def __str__(self):
+        return self.nome
+
+
+class Profissional(models.Model):
+    nome = models.CharField(max_length=255)
+    especialidade = models.CharField(unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
+    numTelefone = models.CharField(max_length=11, unique=True)
+    horaAtendimento = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+    
+# class Vacinacao(models.Model):
+#     nome = models.CharField(max_length=255)
+#     especialidade = models.CharField(unique=True)
+#     cpf = models.CharField(max_length=11, unique=True)
+#     numTelefone = models.CharField(max_length=11, unique=True)
+#     horaAtendimento = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return self.nome
+    
+class Consulta(models.Model):
+    nome = models.CharField(max_length=255)
+    data = models.DateField(max_length=15)
+    horario = models.CharField(max_length=15)
+    tipoAtendimento = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+    
+class Notificacao(models.Model):
+    titulo = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=255)
+    dataEnvio = models.DateField(max_length=15)
+
+    def __str__(self):
+        return self.nome
