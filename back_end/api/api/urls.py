b15b838b6a77b57678c3b73e2674aec08987713c
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-07-10 19:13:11 -0300

    adicionando as urls

--- back_end/api/api/urls.py
+++ back_end/api/api/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from core.views import AdministradorViewSet, PacienteViewSet, ProfissionalViewSet, VacinacaoViewSet, ConsultaViewSet, AvisosViewSet
+
+router = routers.DefaultRouter()
+router.register(r'administrador', AdministradorViewSet)
+router.register(r'pacientes', PacienteViewSet)
+router.register(r'profissionais', ProfissionalViewSet)
+router.register(r'vacinacao', VacinacaoViewSet)
+router.register(r'consultas', ConsultaViewSet)
+router.register(r'avisos', AvisosViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
