d699437728b7cf83de1995f83e4e536fc6926ddc
Isa Kaillany <isakaillany123@gmail.com>
2024-07-24 02:31:38 -0300

    modularizando a homePage + criando rotas

--- front_end/sig_ubs/lib/main.dart
+++ front_end/sig_ubs/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sig_ubs/pages/loginPage.dart';
+import 'package:sig_ubs/pages/menuPage.dart';
+import 'package:sig_ubs/pages/registerPage.dart';
+import 'package:sig_ubs/pages/homePage.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,124 +19,13 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 0, 148, 219)),
         useMaterial3: true,
       ),
-      // home: const MyHomePage(title: 'Página Principal'),
-      home: const LoginPage(),
+      home: const MenuPage(),
+      routes: {
+        '/login': (context) => const LoginPage(), 
+        '/register': (context) => const RegisterPage(), 
+        '/home': (context) => const HomePage(title: 'Página Principal'), 
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 0, 148, 219),
-          title: Text(widget.title),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Conteúdo da página',
-              ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: const MyNewNavBar());
-  }
-}
-
-class MyNewNavBar extends StatefulWidget {
-  const MyNewNavBar({super.key});
-
-  @override
-  State<MyNewNavBar> createState() => _MyNewNavBarState();
-}
-
-class _MyNewNavBarState extends State<MyNewNavBar> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      iconSize: 35.0,
-      onTap: _onItemTapped,
-      currentIndex: _selectedIndex,
-      selectedItemColor: const Color.fromARGB(255, 78, 146, 234),
-      unselectedItemColor: Colors.grey,
-      selectedLabelStyle: const TextStyle(fontSize: 2.0),
-      unselectedLabelStyle: const TextStyle(fontSize: 2.0),
-      items: [
-        BottomNavigationBarItem(
-            label: "",
-            icon: _buildItemIcon(Icons.home_outlined, 0),
-            backgroundColor: const Color.fromARGB(255, 32, 61, 99)),
-        BottomNavigationBarItem(
-            label: "",
-            icon: _buildItemIcon(Icons.bloodtype_outlined, 1),
-            backgroundColor: const Color.fromARGB(255, 32, 61, 99)),
-        BottomNavigationBarItem(
-            label: "",
-            icon: _buildItemIcon(Icons.medical_services_outlined, 2),
-            backgroundColor: const Color.fromARGB(255, 32, 61, 99)),
-        BottomNavigationBarItem(
-            label: "",
-            icon: _buildItemIcon(Icons.notifications_none_outlined, 3),
-            backgroundColor: const Color.fromARGB(255, 32, 61, 99)),
-        BottomNavigationBarItem(
-            label: "",
-            icon: _buildItemIcon(Icons.settings_outlined, 4),
-            backgroundColor: const Color.fromARGB(255, 32, 61, 99)),
-      ],
-    );
-  }
-
-  //Buttons animation
-  Widget _buildItemIcon(IconData iconData, int index) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 100),
-      transform: Matrix4.translationValues(
-        0,
-        _selectedIndex == index
-            ? -15.0
-            : 0.0, // Adjust the translation for selected item
-        0,
-      ),
-      decoration: BoxDecoration(
-        color: _selectedIndex == index
-            ? const Color.fromARGB(255, 255, 255, 255)
-            : Colors.transparent,
-        borderRadius: BorderRadius.circular(50.0),
-        border: Border.all(
-          color: _selectedIndex == index
-              ? const Color.fromARGB(255, 32, 61, 99)
-              : Colors.transparent,
-          width: 5.0,
-        ),
-      ),
-      padding: const EdgeInsets.all(15.0),
-      child: Icon(
-        iconData,
-        color: _selectedIndex == index
-            ? const Color.fromARGB(255, 32, 61, 99)
-            : Colors.blue,
-      ),
-    );
-  }
-}
