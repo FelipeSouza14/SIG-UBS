113347436608b27d2f69e8137a2cbfd21dc41136
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-07-26 22:16:36 -0300

    Configurando o token de acesso e criptografando o nome de usuário

--- back_end/api/core/views.py
+++ back_end/api/core/views.py
@@ -8,6 +8,24 @@ from .serializers import AdministradorSerializer, PacienteSerializer, Profission
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['nome'] = user.nome # Criptografa no token o nome do usuário
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
+
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
