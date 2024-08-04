8cc60ef642b6203f9ea3d76a311473dfefd8b516
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-08-04 19:14:23 -0300

    Salvando os tokens de autenticação no armazenamento local

--- front_end/sig_ubs/lib/context/authProvider.dart
+++ front_end/sig_ubs/lib/context/authProvider.dart
@@ -1,25 +1,53 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+// Essa classe gerencia os estados
 class AuthProvider with ChangeNotifier {
-  late String _authToken = '';
-  Map<String, dynamic> _user = {};
+  late String _authToken = ''; // Esse é o estado para o token de atualização
+  Map<String, dynamic> _user = {}; // Esse é o estado para a decodificação do token de acesso, que seria um objeto JSON, por isso as {}
 
   String get authToken => _authToken;
   Map<String, dynamic> get user => _user;
-  
-  void setAuth(String token) {
+
+  AuthProvider() {
+    _loadAuthData();
+  }
+
+  Future<void> _loadAuthData() async {
+    final prefs = await SharedPreferences.getInstance(); // Essa função busca os dados salvos no armazenamento local(histórico)
+    final token = prefs.getString('refresh');
+    final user = prefs.getString('access');
+
+    if (token != null) {
+      _authToken = token;
+    }
+    if (user != null) {
+      _user = Map<String, dynamic>.from(jsonDecode(user));
+    }
+    notifyListeners();
+  }
+
+  Future<void> setAuth(String token) async {
     _authToken = token;
-    notifyListeners(); // Notifique os ouvintes sobre a alteração
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('refresh', token);
+    notifyListeners();
   }
 
-  void setUser(Map<String, dynamic> user) {
+  Future<void> setUser(Map<String, dynamic> user) async {
     _user = user;
-    notifyListeners(); // Notifique os ouvintes sobre a alteração
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('access', jsonEncode(user));
+    notifyListeners();
   }
 
-  void logout() {
+  Future<void> logout() async {
     _authToken = '';
     _user = {};
-    notifyListeners(); // Notifique os ouvintes sobre a alteração
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('refresh');
+    await prefs.remove('access');
+    notifyListeners();
   }
 }
